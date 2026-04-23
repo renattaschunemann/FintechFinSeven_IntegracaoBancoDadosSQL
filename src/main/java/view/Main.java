1e47d18879437419604570e0d6667dc2c2c6512d
renattaschunemann <renattaschunemann@hotmail.com>
2026-04-23 18:50:16 -0300

    conexao banco de dados com o padrao de projetos factory

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -1,4 +1,18 @@
 package view;
+import factory.ConnectionFactory;
 
-public class Main {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Main
+{
+    public static void main( String[] args )
+    {
+        try {
+            Connection conexao = ConnectionFactory.getConnection();
+            System.out.println("Conexão realizada!");
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
