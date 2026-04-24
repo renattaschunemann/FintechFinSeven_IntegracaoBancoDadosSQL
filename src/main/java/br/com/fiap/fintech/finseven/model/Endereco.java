689537f7d4771f1e74b8c24cc5267e553d218516
Kendra Riquena <kendracruz29@gmail.com>
2026-04-24 17:19:45 -0300

    Inclusão das classes model

--- src/main/java/br/com/fiap/fintech/finseven/model/Endereco.java
+++ src/main/java/br/com/fiap/fintech/finseven/model/Endereco.java
@@ -1,4 +1,87 @@
 package br.com.fiap.fintech.finseven.model;
 
 public class Endereco {
+    private String logradouro;
+    private int numero;
+    private int cep;
+    private String cidade;
+    private String estado;
+
+
+    public Endereco() {}
+
+    public Endereco(String logradouro, int numero, int cep, String cidade, String estado) {
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.estado = estado;
+
+
+    }
+
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" +
+                "logradouro='" + logradouro + '\'' +
+                ", numero=" + numero +
+                ", cep=" + cep +
+                ", cidade='" + cidade + '\'' +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
+
+    public void exibirEndereco(){
+
+        System.out.println("Logradouro: " + logradouro
+                +"\nNúmero: " + numero
+                + "\nCep: "+ cep
+                + "\nCidade: " + cidade
+                + "\nEstado: " + estado
+        );
+
+    }
+
 }
+
--- src/main/java/br/com/fiap/fintech/finseven/model/Login.java
+++ src/main/java/br/com/fiap/fintech/finseven/model/Login.java
@@ -1,4 +1,123 @@
 package br.com.fiap.fintech.finseven.model;
 
 public class Login {
+    private int idLogin;
+    private String email;
+    private String senha;
+    private int tentativasLogin;
+    private boolean contaBloqueada;
+    private  Usuario usuario;
+
+
+    public Login() {
+    }
+
+    public Login(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Login(int idLogin) {
+        System.out.println(idLogin);
+    }
+
+    public Login(int idLogin, String email, String senha, int tentativasLogin, boolean contaBloqueada) {
+        this.idLogin = idLogin;
+        this.email = email;
+        this.senha = senha;
+        this.tentativasLogin = tentativasLogin;
+        this.contaBloqueada = contaBloqueada;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getIdLogin() {
+        return idLogin;
+    }
+
+    public void setIdLogin(int idLogin) {
+        this.idLogin = idLogin;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getTentativasLogin() {
+        return tentativasLogin;
+    }
+
+    public void setTentativasLogin(int tentativasLogin) {
+        this.tentativasLogin = tentativasLogin;
+    }
+
+    public boolean isContaBloqueada() {
+        return contaBloqueada;
+    }
+
+    public void setContaBloqueada(boolean contaBloqueada) {
+        this.contaBloqueada = contaBloqueada;
+    }
+
+    //Método validarCredenciais executado
+    public boolean validarCredenciais(String email, String senha) {
+        System.out.println("Verificando email e senha");
+        return email.equals(email) && senha.equals(senha);
+    }
+
+    //Método realizarLogin
+    public void realizarLogin(String emailInformado, String senhaInformada) {
+        if (this.contaBloqueada) {
+            System.out.println("ACESSO NEGADO: Conta bloqueada. Procure o suporte.");
+            return;
+        }
+
+        // Validação: Verifica se a senha informada é IGUAL à senha do objeto
+        if (this.senha != null && this.senha.equals(senhaInformada)) {
+            System.out.println("Login realizado com sucesso! Bem-vindo, " + (usuario != null ? usuario.getNome() : emailInformado));
+            this.tentativasLogin = 0;
+        } else {
+            this.tentativasLogin++;
+            System.out.println("Erro: Senha incorreta! Tentativa " + tentativasLogin + " de 3.");
+
+            if (this.tentativasLogin >= 3) {
+                bloquearConta();
+            }
+        }
+    }
+
+    //Método bloquearConta executado
+    public void bloquearConta() {
+        contaBloqueada = true;
+        System.out.println("Conta bloqueada por excesso tentativas inválidas");
+    }
+
+    //Método recuperarSenha executado
+    public void recuperarSenha() {
+        System.out.println("Processo de recuperação iniciado");
+    }
+
+    //Método contatarSuporte executado
+    public void contatarSuporte() {
+        System.out.println("Direcionando usuário ao suporte");
+    }
+
 }
