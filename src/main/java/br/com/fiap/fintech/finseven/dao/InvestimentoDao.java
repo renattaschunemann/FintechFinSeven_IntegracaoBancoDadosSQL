28759d17bdf52e4cdf127b33c8e7db9027510c45
Kendra Riquena <kendracruz29@gmail.com>
2026-04-26 16:16:04 -0300

    Investimento DAO

--- src/main/java/br/com/fiap/fintech/finseven/dao/InvestimentoDao.java
+++ src/main/java/br/com/fiap/fintech/finseven/dao/InvestimentoDao.java
@@ -2,7 +2,6 @@ package br.com.fiap.fintech.finseven.dao;
 
 import br.com.fiap.fintech.finseven.factory.ConnectionFactory;
 import br.com.fiap.fintech.finseven.model.Investimento;
-import br.com.fiap.fintech.finseven.model.Receita;
 import br.com.fiap.fintech.finseven.model.Transacao;
 
 import java.sql.Connection;
@@ -20,92 +19,93 @@ public class InvestimentoDao {
     public InvestimentoDao() throws SQLException {
         conexao = ConnectionFactory.getConnection();
         transacaoDao = new TransacaoDao();
+
     }
 
     public void insert(Investimento investimento) throws SQLException {
-        transacaoDao.cadastrar(investimento);
+        transacaoDao.insert(investimento);
 
-        PreparedStatement stmt = conexao.prepareStatement("INSERT INTO T_FINSEVEN_RECEITA (ID_TRANSACAO, ORIGEM_RECEITA) VALUES (?, ?)");
+        PreparedStatement stmt = conexao.prepareStatement("INSERT INTO T_FINSEVEN_INVESTIMENTO (ID_TRANSACAO, TX_INVESTIMENTO) VALUES (?, ?)");
 
-        stmt.setLong(1, receita.getId());
-        stmt.setString(2, receita.getOrigem());
+        stmt.setLong(1, investimento.getId());
+        stmt.setDouble(2, investimento.getTaxa());
 
         stmt.executeUpdate();
-        System.out.println("Receita cadastrada com sucesso!");
+        System.out.println("Investimento cadastrado com sucesso!");
         stmt.close();
     }
 
-    public void update(Receita receita) throws SQLException {
-        transacaoDao.update(receita);
+    public void update(Investimento investimento) throws SQLException {
+        transacaoDao.update(investimento);
 
-        PreparedStatement stmt = conexao.prepareStatement("UPDATE T_FINSEVEN_RECEITA SET ORIGEM_RECEITA = ? " +
+        PreparedStatement stmt = conexao.prepareStatement("UPDATE T_FINSEVEN_INVESTIMENTO SET TX_INVESTIMENTO = ? " +
                 "WHERE ID_TRANSACAO = ?");
-        stmt.setString(1, receita.getOrigem());
-        stmt.setLong(2, receita.getId());
+        stmt.setDouble(1, investimento.getTaxa());
+        stmt.setLong(2, investimento.getId());
 
         int linhas = stmt.executeUpdate();
         if (linhas > 0) {
-            System.out.println("Receita atualizada com sucesso!");
+            System.out.println("Investimento atualizado com sucesso!");
         } else {
-            System.out.println("Nenhuma receita encontrada com o ID: " + receita.getId());
+            System.out.println("Nenhum investimento encontrado com o ID: " + investimento.getId());
         }
     }
 
-    public Receita searchById(long id) throws SQLException {
+    public Investimento searchById(long id) throws SQLException {
         Transacao transacao = transacaoDao.searchById(id);
 
-        PreparedStatement stmt = conexao.prepareStatement("SELECT * FROM T_FINSEVEN_RECEITA WHERE ID_TRANSACAO = ?");
+        PreparedStatement stmt = conexao.prepareStatement("SELECT * FROM T_FINSEVEN_INVESTIMENTO WHERE ID_TRANSACAO = ?");
         stmt.setLong(1, id);
 
         ResultSet rs = stmt.executeQuery();
-        Receita receita = null;
+        Investimento investimento = null;
 
         if (rs.next()) {
-            receita = new Receita(transacao);
-            receita.setOrigem(rs.getString("ORIGEM_RECEITA"));
+            investimento = new Investimento(transacao);
+            investimento.setTaxa(rs.getDouble("TX_INVESTIMENTO"));
         }
 
-        return receita;
+        return investimento;
     }
 
-    public List<Receita> getAll() throws SQLException {
-        List<Receita> lista = new ArrayList<>();
-        // SQL para selecionar todos os registros das tabelas do join entre transacao e receita
-        String sql = "SELECT * FROM T_FINSEVEN_RECEITA r INNER JOIN T_FINSEVEN_TRANSACAO t ON r.ID_TRANSACAO = t.ID_TRANSACAO ORDER BY t.DT_TRANSACAO DESC";
+    public List<Investimento> getAll() throws SQLException {
+        List<Investimento> lista = new ArrayList<>();
+        // SQL para selecionar todos os registros das tabelas do join entre transacao e investimento
+        String sql = "SELECT * FROM T_FINSEVEN_INVESTIMENTO i INNER JOIN T_FINSEVEN_TRANSACAO t ON i.ID_TRANSACAO = t.ID_TRANSACAO ORDER BY t.DT_TRANSACAO DESC";
 
         try (PreparedStatement stmt = conexao.prepareStatement(sql);
              ResultSet rs = stmt.executeQuery()) {
 
             // O while percorre todas as linhas retornadas pelo banco
             while (rs.next()) {
-                Receita receita = new Receita();
-                receita.setId(rs.getLong("ID_TRANSACAO"));
-                receita.setIdBanco(rs.getLong("ID_BANCO"));
-                receita.setIdCategoria(rs.getLong("ID_CATEGORIA"));
-                receita.setValor(rs.getDouble("VLR_TRANSACAO"));
-                receita.setOrigem(rs.getString("ORIGEM_RECEITA"));
+                Investimento investimento = new Investimento();
+                investimento.setId(rs.getLong("ID_TRANSACAO"));
+                investimento.setIdBanco(rs.getLong("ID_BANCO"));
+                investimento.setIdCategoria(rs.getLong("ID_CATEGORIA"));
+                investimento.setValor(rs.getDouble("VLR_TRANSACAO"));
+                investimento.setTaxa(rs.getDouble("TX_INVESTIMENTO"));
 
                 // Converte a data do banco para LocalDate conforme seu modelo [cite: 126]
                 if (rs.getDate("DT_TRANSACAO") != null) {
-                    receita.setData(rs.getDate("DT_TRANSACAO").toLocalDate());
+                    investimento.setData(rs.getDate("DT_TRANSACAO").toLocalDate());
                 }
 
-                receita.setDescricao(rs.getString("DS_TRANSACAO"));
-                receita.setTipo(rs.getString("TP_TRANSACAO"));
+                investimento.setDescricao(rs.getString("DS_TRANSACAO"));
+                investimento.setTipo(rs.getString("TP_TRANSACAO"));
 
                 // Adiciona o objeto preenchido na lista
-                lista.add(receita);
+                lista.add(investimento);
             }
         }
         return lista;
     }
 
     public void remove(long id) throws SQLException {
-        String sqlRec = "DELETE FROM T_FINSEVEN_RECEITA WHERE ID_TRANSACAO = ?";
+        String sqlInvest = "DELETE FROM T_FINSEVEN_INVESTIMENTO WHERE ID_TRANSACAO = ?";
         String sqlTrans = "DELETE FROM T_FINSEVEN_TRANSACAO WHERE ID_TRANSACAO = ?";
         try {
             conexao.setAutoCommit(false);
-            try (PreparedStatement stmt = conexao.prepareStatement(sqlRec)) {
+            try (PreparedStatement stmt = conexao.prepareStatement(sqlInvest)) {
                 stmt.setLong(1, id);
                 stmt.executeUpdate();
             }
